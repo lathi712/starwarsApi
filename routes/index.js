b7b1e157c052a915da5224ad50339e3c9c1265d2
Lathesh Karkera S <karkeralathesh@gmail.com>
2017-04-21 12:36:16 +0530

    characters limit and sort

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,43 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 
+
+var resArr =[];
+var pushArr=[];
 router.get('/api/characters',function (req,res) {
     request('http://swapi.co/api/people/', function (error, response, body) {
         var bodyReq = JSON.parse(body);
         var arr = bodyReq.results;
         var sort = req.query.sort;
-        console.log(sort);
+        for(var i=0;i<5;i++){
+            resArr.push(arr);
+
+        }
+        
+        function flatten(arr) {
+            var result = [];
+            var resultArr = function resultArr(arg) {
+                if (!Array.isArray(arg)) {
+                    result.push(arg);
+                } else {
+                    for (var a in arg) {
+
+                        resultArr(arg[a]);
+                    }
+                }
+            };
+            resultArr(arr);
+            return result;
+        }
+
+        pushArr = flatten(resArr);
+
         if(sort==null){
             if(error){
                 res.sendStatus(400)
             }else{
                 //res.render('pages/index', { peoples : arr });
-                res.json(arr);
+                res.json(pushArr);
             }
         }else {
 
@@ -25,23 +50,24 @@ router.get('/api/characters',function (req,res) {
                         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
                     });
                 }
-                newArray = sortByKey(arr,'name');
+                newArray = sortByKey(pushArr,'name');
             }else if(sort==='mass'){
-                newArray = arr.sort(function(a, b){
+                newArray = pushArr.sort(function(a, b){
                     return a.mass - b.mass;
                 });
             }else if(sort==='height'){
-                newArray = arr.sort(function(a, b){
+                newArray = pushArr.sort(function(a, b){
                     return a.height - b.height;
                 });
             }
+            if(error){
+                res.sendStatus(400)
+            }else{
+                res.json(newArray);
+                //   res.render('pages/index', { peoples : newArray });
+            }
         }
-        if(error){
-            res.sendStatus(400)
-        }else{
-           res.json(newArray);
-         //   res.render('pages/index', { peoples : newArray });
-        }
+
 
     });
 });
